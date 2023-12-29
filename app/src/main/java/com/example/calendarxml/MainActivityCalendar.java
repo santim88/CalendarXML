00b2976b5c29a2736763d0ab64e45bdca3df1ad4
Santiago Correa <sacorreac@unal.edu.co>
2023-12-28 22:23:14 -0500

    Initial commit

---
+++ app/src/main/java/com/example/calendarxml/MainActivityCalendar.java
@@ -0,0 +1,51 @@
+package com.example.calendarxml;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.widget.CalendarView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
+public class MainActivityCalendar extends AppCompatActivity {
+
+    CalendarView calendarView;
+    Calendar calendar;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.activity_main);
+        calendar = Calendar.getInstance();
+
+        setDate(3,1,2023);
+        getDate();
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView calendarView, int year, int month, int day) {
+                Toast.makeText(MainActivityCalendar.this, day+"/"+month+"/"+year, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    public void setDate(int day, int month, int year){
+        calendar.set(Calendar.YEAR, year);
+        calendar.set(Calendar.MONTH, month-1);
+        calendar.set(Calendar.DAY_OF_MONTH, day);
+        long milli = calendar.getTimeInMillis();
+        calendarView.setDate(milli);
+    }
+
+    public void getDate(){
+        Long date = calendarView.getDate();
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yy", Locale.getDefault());
+        String selected_date = simpleDateFormat.format(calendar.getTime());
+        Toast.makeText(this, selected_date, Toast.LENGTH_SHORT).show();
+    }
+}
